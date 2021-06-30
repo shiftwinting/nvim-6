f96b3cd3b51f78db9659f7d05240256f199c464a
wongxy <xiyao.wong@foxmail.com>
2021-06-30 17:26:26 +0800

    chore(colorscheme): update settings

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -8,13 +8,26 @@ local theme_configs = {
   sonokai = function()
     vim.g.sonokai_better_performance = 1
     vim.g.sonokai_style = "atlantis"
+    vim.g.sonokai_sign_column_background = 'none'
+    vim.g.sonokai_diagnostic_text_highlight = 1
+    -- vim.g.sonokai_diagnostic_line_highlight = 1
+    vim.g.sonokai_diagnostic_virtual_text = 'colored'
   end,
   edge = function()
     vim.g.edge_better_performance = 1
     vim.g.edge_style = "aura"
+    vim.g.edge_sign_column_background = 'none'
+    vim.g.edge_diagnostic_text_highlight = 1
+    -- vim.g.edge_diagnostic_line_highlight = 1
+    vim.g.edge_diagnostic_virtual_text = 'colored'
   end,
   ["gruvbox-material"] = function()
+    vim.g.gruvbox_material_better_performance = 1
     vim.g.gruvbox_material_background = "hard"
+    vim.g.gruvbox_material_sign_column_background = 'none'
+    vim.g.gruvbox_material_diagnostic_text_highlight = 1
+    -- vim.g.gruvbox_material_diagnostic_line_highlight = 1
+    vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
   end,
 }
 
