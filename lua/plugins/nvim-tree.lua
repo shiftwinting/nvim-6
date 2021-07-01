2e21b40f432647ff549bd600274e8f569163252c
wongxy <xiyao.wong@foxmail.com>
2021-07-01 15:14:02 +0800

    fix: config for nvim-tree keybinds

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,14 @@
+local tree_cb = require("nvim-tree.config").nvim_tree_callback
+
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_ignore = { ".git", "node_modules", ".cache", "__pycache__" }
 vim.g.nvim_tree_bindings = {
-  ["s"] = ":lua require'nvim-tree'.on_keypress('vsplit')<CR>",
-  ["i"] = ":lua require'nvim-tree'.on_keypress('split')<CR>",
+  { key = "s", cb = tree_cb("vsplit") },
+  { key = "i", cb = tree_cb("split") },
 }
+
 vim.g.nvim_tree_icons = {
   default = "",
   symlink = "",
