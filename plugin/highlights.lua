64587679401cc7d467609adf6d39773890bc1710
wongxy <xiyao.wong@foxmail.com>
2021-07-02 10:38:01 +0800

    update highlights

--- plugin/highlights.lua
+++ plugin/highlights.lua
@@ -1,8 +1,4 @@
-local function set()
-  wxy.highlight_all({
-    { "NvimNotificationError", { guifg = "#ec5f67" } },
-    { "NvimNotificationInfo", { guifg = "#51afef" } },
-  })
-end
-
-wxy.autocmd({ { "ColorScheme", set, "*" } })
+wxy.highlight_all({
+  { "NvimNotificationError", { guifg = "#ec5f67" } },
+  { "NvimNotificationInfo", { guifg = "#51afef" } },
+})
