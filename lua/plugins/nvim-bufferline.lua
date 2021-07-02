5dfe21139f02fe58e5b5fa679fe2ec2e5b1ec421
wongxy <xiyao.wong@foxmail.com>
2021-07-02 20:50:11 +0800

    chore(bufferline): add offsets

--- lua/plugins/nvim-bufferline.lua
+++ lua/plugins/nvim-bufferline.lua
@@ -2,8 +2,12 @@ require("bufferline").setup({
   options = {
     modified_icon = "✥",
     show_buffer_close_icons = false,
-    show_close_icon = true,
+    show_close_icon = false,
     mappings = true,
-    always_show_bufferline = false,
+    always_show_bufferline = true,
+    offsets = {
+      { filetype = "NvimTree", text = "FILE EXPLORER", text_align = "center" },
+      { filetype = "vista", text = "SYMBOLS AND TAGS", text_align = "center" },
+    },
   },
 })
