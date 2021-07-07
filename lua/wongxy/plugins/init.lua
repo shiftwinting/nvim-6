80fe00d2a673ec98cd6cfbed2d07019e29dca524
wongxy <xiyao.wong@foxmail.com>
2021-07-07 17:53:03 +0800

    refactor(packer): clearer structure

--- lua/wongxy/plugins/init.lua
+++ lua/wongxy/plugins/init.lua
@@ -5,9 +5,6 @@ end
 vim.cmd([[packadd packer.nvim]])
 
 ---------------------------------------
-local function conf(name)
-  return string.format("require('wongxy.plugins.%s')", name)
-end
 
 local loaded = false
 local function load()
@@ -23,175 +20,15 @@ local function load()
     disable_commands = true,
   })
   packer.startup(function(use)
-    -- packer
-    use({ "wbthomason/packer.nvim", opt = true })
-
-    use({ "kyazdani42/nvim-web-devicons" })
-    -- colorscheme
-    use({
-      { "sainnhe/sonokai" },
-      { "sainnhe/edge" },
-      { "sainnhe/gruvbox-material" },
-      { "monsonjeremy/onedark.nvim" },
-      { "xiyaowong/nvim-transparent", config = conf("transparent") },
-    })
-    -- File Tree
-    use({
-      "kyazdani42/nvim-tree.lua",
-      cmd = { "NvimTreeToggle", "NvimTreeOpen" },
-      config = conf("nvim-tree"),
-    })
-    -- UI
-    use({
-      { "akinsho/nvim-bufferline.lua", config = conf("nvim-bufferline") },
-      {
-        "lukas-reineke/indent-blankline.nvim",
-        event = { "BufRead", "InsertEnter" },
-        config = conf("indent-blakline"),
-      },
-    })
-    -- lsp, completion
-    use({
-      {
-        "neoclide/coc.nvim",
-        branch = "release",
-        event = { "InsertEnter", "BufRead" },
-        config = conf("coc"),
-      },
-      "honza/vim-snippets",
-    })
-    -- Enhance
-    use("xiyaowong/nvim-cursorword")
-    use({
-      "antoinemadec/FixCursorHold.nvim",
-      -- Vim-cool disables search highlighting when you are done searching
-      -- and re-enables it when you search again.
-      { "romainl/vim-cool", event = "BufRead" },
-      { "rhysd/accelerated-jk" },
-      { "kana/vim-niceblock", event = "BufRead" },
-      {
-        "nacro90/numb.nvim",
-        event = "CmdlineEnter",
-        config = function()
-          require("numb").setup()
-        end,
-      },
-      {
-        "karb94/neoscroll.nvim",
-        event = "BufRead",
-        keys = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "zt", "zz", "zb" },
-        config = function()
-          require("neoscroll").setup({
-            mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "zt", "zz", "zb" },
-          })
-        end,
-      },
-    })
-
-    use({ "junegunn/vim-easy-align", event = { "BufRead", "BufEnter" } })
-    use({ "talek/obvious-resize", config = "vim.g.obvious_resize_default = 2" }) -- resize window
-    use({
-      "b3nj5m1n/kommentary",
-      event = { "BufRead", "InsertEnter" },
-      config = function()
-        require("kommentary.config").configure_language(
-          "lua",
-          { prefer_single_line_comments = true }
-        )
-      end,
-    })
-    use({
-      "blackCauldron7/surround.nvim",
-      event = { "BufRead", "InsertEnter" },
-      config = function()
-        require("surround").setup({
-          mappings_style = "surround",
-        })
-      end,
-    })
-    --Movement
-    use({
-      { "rhysd/clever-f.vim", event = "BufRead" },
-      {
-        "phaazon/hop.nvim",
-        cmd = { "HopWord", "HopLine", "HopPattern", "HopChar1" },
-        config = function()
-          require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-        end,
-      },
-    })
-    use({
-      "norcalli/nvim-colorizer.lua",
-      event = { "BufRead", "BufEnter" },
-      config = function()
-        require("colorizer").setup({ "*" }, {
-          names = false,
-          mode = "background",
-        })
-      end,
-    })
-    use({
-      "windwp/nvim-autopairs",
-      event = "InsertEnter",
-      config = function()
-        require("nvim-autopairs").setup({
-          close_triple_quotes = true,
-          check_ts = false,
-        })
-      end,
-    })
-    use({
-      "TimUntersberger/neogit",
-      requires = "nvim-lua/plenary.nvim",
-      cmd = "Neogit",
-      config = conf("neogit"),
-    })
-    use({
-      "lewis6991/gitsigns.nvim",
-      event = { "BufRead", "BufNewFile" },
-      requires = "nvim-lua/plenary.nvim",
-      config = conf("gitsigns"),
-    })
-    use({
-      "iamcco/markdown-preview.nvim",
-      run = ":call mkdp#util#install()",
-      ft = { "markdown" },
-      config = function()
-        vim.g.mkdp_auto_start = 0
-        vim.g.mkdp_auto_close = 1
-      end,
-    })
-    -- treesitter
-    use({
-      {
-        "nvim-treesitter/nvim-treesitter",
-        event = "BufRead",
-        config = conf("nvim-treesitter"),
-      },
-      { "p00f/nvim-ts-rainbow", after = "nvim-treesitter" },
-      { "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" },
-    })
-
-    use({ "liuchengxu/vista.vim", cmd = "Vista", config = conf("vista") })
-    use({
-      "akinsho/nvim-toggleterm.lua",
-      cmd = "ToggleTerm",
-      config = conf("nvim-toggleterm"),
-    })
-    use({ "sindrets/diffview.nvim", cmd = "DiffviewOpen", config = conf("diffview") })
-
-    use({
-      {
-        "nvim-telescope/telescope.nvim",
-        cmd = "Telescope",
-        config = conf("telescope"),
-        requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
-      },
-      "fannheyward/telescope-coc.nvim",
-      "xiyaowong/telescope-emoji.nvim",
-    })
-
-    use({ "tweekmonster/startuptime.vim", cmd = "StartupTime" })
+    local plugins = require("wongxy.plugins.use")
+    for name, plugin in pairs(plugins) do
+      if type(plugin) == "table" then
+        table.insert(plugin, name)
+        use(plugin)
+      else
+        use(plugin)
+      end
+    end
   end)
 end
 
