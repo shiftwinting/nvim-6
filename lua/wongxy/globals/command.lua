a47e983a59ca196fad6643aac5c12018aa2441e3
wongxy <xiyao.wong@foxmail.com>
2021-07-10 23:47:13 +0800

    feat(globals): add command

---
+++ lua/wongxy/globals/command.lua
@@ -0,0 +1,15 @@
+---Create an nvim command
+---@param args table
+function wxy.command(args)
+  local nargs = args.nargs or 0
+  local name = args[1]
+  local rhs = args[2]
+  local types = (args.types and type(args.types) == "table") and table.concat(args.types, " ") or ""
+
+  if type(rhs) == "function" then
+    local fn_id = wxy._create(rhs)
+    rhs = string.format("lua wxy._execute(%d%s)", fn_id, nargs > 0 and ", <f-args>" or "")
+  end
+
+  vim.cmd(string.format("command! -nargs=%s %s %s %s", nargs, types, name, rhs))
+end
