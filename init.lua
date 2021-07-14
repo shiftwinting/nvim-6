efc22d89005179bc01976a896657fac644d5adb7
wongxy <xiyao.wong@foxmail.com>
2021-07-14 20:43:18 +0800

    refacor: load plugins,statusline module asynchronously

--- init.lua
+++ init.lua
@@ -4,9 +4,12 @@ vim.cmd 'syntax enable'
 
 require 'wongxy.globals'
 require 'wongxy.options'
-require 'wongxy.plugins'
-require 'wongxy.statusline'
 
--- load local profile
-pcall(vim.cmd, 'source ' .. CONFIG_PATH .. 'profile.vim')
-pcall(vim.cmd, 'luafile ' .. CONFIG_PATH .. 'profile.lua')
+vim.schedule(function()
+  require 'wongxy.plugins'
+  require 'wongxy.statusline'
+
+  -- load local profile
+  pcall(vim.cmd, 'source ' .. CONFIG_PATH .. 'profile.vim')
+  pcall(vim.cmd, 'luafile ' .. CONFIG_PATH .. 'profile.lua')
+end)
