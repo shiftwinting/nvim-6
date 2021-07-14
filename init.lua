a86fffb569a94bb036e50ad5d1d8581d132f63ba
wongxy <xiyao.wong@foxmail.com>
2021-07-14 17:11:23 +0800

    fix: correct profile. settings path

--- init.lua
+++ init.lua
@@ -8,5 +8,5 @@ require 'wongxy.plugins'
 require 'wongxy.statusline'
 
 -- load local profile
-pcall(vim.cmd, 'source  profile.vim')
-pcall(vim.cmd, 'luafile profile.lua')
+pcall(vim.cmd, 'source ' .. CONFIG_PATH .. 'profile.vim')
+pcall(vim.cmd, 'luafile ' .. CONFIG_PATH .. 'profile.lua')
